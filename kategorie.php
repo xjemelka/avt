bdd709b725ed4ff2e4dc39a31baf51b91fc86635
xjemelka <xjemelka@mendelu.cz>
2018-11-25 19:20:36 +0100

    v15.2 zdrojova databaze s otazkami prevedena plne do nastaveni
    zadani_knihy_otazky a další takové plně exportovány do nastavení pro
    zachování normálních forem. Ve verzi 15.1 došlo k přesunu u generování
    dat, tady je upraveno nastavení kategorií, otázek a další nastavení
    zadání učitelem.

--- kategorie.php
+++ kategorie.php
@@ -8,10 +8,10 @@ require 'overeni.php';
 /* @tpl Latte\Engine */
 
 //ověřuje jestli je učitel - později přesunout takové podmínky do souborů jako je overeni.php pro každé oprávnění zvlášť
-if ($_SESSION["user"]["typ"] != 1 or empty($_SESSION['db'])){
+if ($_SESSION["user"]["typ"] != 1 or empty($_SESSION['db']) or empty($_SESSION['id_db'])){
     header('Location: index.php');
 }
-    $db->query("USE ".$_SESSION['db']."_otazky");
+    //$db->query("USE ".$_SESSION['db']."_otazky");
     
     if(!empty($_POST['kategorie'])){
         try {
@@ -26,12 +26,14 @@ if ($_SESSION["user"]["typ"] != 1 or empty($_SESSION['db'])){
             foreach ($kategorie as $kat){
                 $kat = trim($kat);
                 if (strlen($kat)>0){
-                    $existuje = $db -> prepare("select count(*) pocet from kategorie where nazev = :kat");
+                    $existuje = $db -> prepare("select count(*) pocet from kategorie where id_zadani = :zad and nazev = :kat");
+                    $existuje->bindValue(":zad", $_SESSION['id_db']);
                     $existuje->bindValue(":kat", $kat);
                     $existuje->execute();
                     $existuje = $existuje->fetch();
                     if ($existuje['pocet']==0){
-                        $kategorie = $db->prepare('INSERT into kategorie (nazev, body, otazek) values (:kat,1,1)');
+                        $kategorie = $db->prepare('INSERT into kategorie (id_zadani, nazev, body, otazek) values (:zad,:kat,1,1)');
+                        $kategorie -> bindValue(":zad", $_SESSION['id_db']);
                         $kategorie -> bindValue(":kat",$kat);
                         $kategorie -> execute();
                     }
@@ -42,22 +44,25 @@ if ($_SESSION["user"]["typ"] != 1 or empty($_SESSION['db'])){
             die($e->getMessage());
         }
     }
-    if(!empty($_POST['smaz_kategorie_id'])){
+    if(!empty($_POST['smaz_id_kategorie'])){
          try {
-             $smaz = $db->prepare("DELETE FROM dotazy WHERE kategorie = :id");
-             $smaz -> bindValue(":id",$_POST['smaz_kategorie_id']);
+             $smaz = $db->prepare("DELETE FROM dotazy WHERE id_kategorie = :id");
+             $smaz -> bindValue(":id",$_POST['smaz_id_kategorie']);
              $smaz -> execute();
-             $smaz = $db->prepare("DELETE FROM kategorie WHERE kategorie_id = :id");
-             $smaz -> bindValue(":id",$_POST['smaz_kategorie_id']);
+             $smaz = $db->prepare("DELETE FROM kategorie WHERE id_kategorie = :id");
+             $smaz -> bindValue(":id",$_POST['smaz_id_kategorie']);
              $smaz -> execute();
              header('Location: '.$_SERVER['PHP_SELF']);
          } catch (Exception $ex) {
             die($ex->getMessage());
          }
     }
-        $kategorie = $db->query("SELECT k.kategorie_id, k.nazev, k.otazek, k.body, count(d.dotazy_id) pocet FROM `kategorie` k
-                                left join dotazy d on k.kategorie_id = d.kategorie
-                                group by k.kategorie_id, k.nazev, k.otazek, k.body ");
+        $kategorie = $db->prepare("SELECT k.id_kategorie, k.nazev, k.otazek, k.body, count(d.id_dotazy) pocet FROM `kategorie` k
+                                left join dotazy d on k.id_kategorie = d.id_kategorie
+                                where k.id_zadani = :zad
+                                group by k.id_kategorie, k.nazev, k.otazek, k.body ");
+        $kategorie -> bindValue(":zad", $_SESSION['id_db']);
+        $kategorie -> execute();
     
     $tplVars["zadani"] = $_SESSION['db'];
     $tplVars["kategorie"] = $kategorie->fetchAll();
--- otazky.php
+++ otazky.php
@@ -11,16 +11,20 @@ require 'overeni.php';
 if ($_SESSION["user"]["typ"] != 1 or ((empty($_SESSION['db']) or empty($_SESSION['kategorie'])) and (empty($_GET['zadani']) or empty($_GET['kategorie'])))){
     header('Location: index.php');
 }
-    if (empty($_SESSION['db']) || empty($_SESSION['kategorie'])){
-        $_SESSION['db']=$_GET['zadani'];
-        $_SESSION['kategorie']=$_GET['kategorie'];
-    }
+    //if (empty($_SESSION['db']) || empty($_SESSION['kategorie'])){
+        //$_SESSION['db']=$_GET['zadani'];
+        //$_SESSION['kategorie']=$_GET['kategorie'];
+    //}
+        
+        if (!empty($_GET['kategorie'])){
+            $_SESSION['kategorie']=$_GET['kategorie'];
+        }
     
-    $db->query("USE ".$_SESSION['db']."_otazky");
+    //$db->query("USE ".$_SESSION['db']."_otazky");
     
     if ( !empty($_POST['nazev']) ) {
         try {
-            $kategorie = $db->prepare('UPDATE kategorie set nazev=:naz,otazek=:ota,body=:bod where kategorie_id=:kat');
+            $kategorie = $db->prepare('UPDATE kategorie set nazev=:naz,otazek=:ota,body=:bod where id_kategorie=:kat');
             $kategorie -> bindValue(":naz",$_POST['nazev']);
             $kategorie -> bindValue(":ota",$_POST['otazek']);
             $kategorie -> bindValue(":bod",$_POST['body']);
@@ -35,24 +39,24 @@ if ($_SESSION["user"]["typ"] != 1 or ((empty($_SESSION['db']) or empty($_SESSION
     
     if ( !empty($_POST['update']) ) {
         try {
-            $otazky = $db->prepare("SELECT dotazy_id from dotazy where kategorie = :kat");
+            $otazky = $db->prepare("SELECT id_dotazy from dotazy where id_kategorie = :kat");
             $otazky -> bindValue(":kat",$_SESSION['kategorie']);    
             $otazky -> execute();
             $otazky = $otazky ->fetchAll();
             
             foreach ($otazky as $otazka){
-                $text = ($_POST['text'.$otazka['dotazy_id']]);
-                $sql = ($_POST['sql'.$otazka['dotazy_id']]);
+                $text = ($_POST['text'.$otazka['id_dotazy']]);
+                $sql = ($_POST['sql'.$otazka['id_dotazy']]);
                 if (strlen(trim($text))>0 && strlen(trim($sql))>0){
-                    $dotaz = $db->prepare('UPDATE dotazy set text=:text,s_q_l=:sql where dotazy_id = :dot');
+                    $dotaz = $db->prepare('UPDATE dotazy set text=:text,s_q_l=:sql where id_dotazy = :dot');
                     $dotaz -> bindValue(":text",$text);
                     $dotaz -> bindValue(":sql",$sql);
-                    $dotaz -> bindValue(":dot",$otazka['dotazy_id']);
+                    $dotaz -> bindValue(":dot",$otazka['id_dotazy']);
                     $dotaz -> execute();
                 }
                 else{
-                    $smaz = $db->prepare("DELETE FROM dotazy WHERE dotazy_id = :id");
-                    $smaz -> bindValue(":id",$otazka['dotazy_id']);
+                    $smaz = $db->prepare("DELETE FROM dotazy WHERE id_dotazy = :id");
+                    $smaz -> bindValue(":id",$otazka['id_dotazy']);
                     $smaz -> execute();
                 }
             }            
@@ -67,7 +71,7 @@ if ($_SESSION["user"]["typ"] != 1 or ((empty($_SESSION['db']) or empty($_SESSION
         try {
             $text = ($_POST['text']);
             $sql = ($_POST['sql']);
-            $dotaz = $db->prepare('INSERT into dotazy (text,s_q_l,kategorie) values (:text,:sql,:kat)');
+            $dotaz = $db->prepare('INSERT into dotazy (text,s_q_l,id_kategorie) values (:text,:sql,:kat)');
             $dotaz -> bindValue(":text",$_POST['text']);
             $dotaz -> bindValue(":sql",$_POST['sql']);
             $dotaz -> bindValue(":kat",$_SESSION['kategorie']);
@@ -78,11 +82,11 @@ if ($_SESSION["user"]["typ"] != 1 or ((empty($_SESSION['db']) or empty($_SESSION
         }
     }
     
-        $otazky = $db->prepare("SELECT dotazy_id, text, s_q_l from dotazy where kategorie = :kat");
+        $otazky = $db->prepare("SELECT id_dotazy, text, s_q_l from dotazy where id_kategorie = :kat");
         $otazky -> bindValue(":kat",$_SESSION['kategorie']);    
         $otazky -> execute();
         
-        $kategorie = $db->prepare("SELECT nazev, otazek, body from kategorie where kategorie_id = :kat");
+        $kategorie = $db->prepare("SELECT nazev, otazek, body from kategorie where id_kategorie = :kat");
         $kategorie -> bindValue(":kat",$_SESSION['kategorie']);    
         $kategorie -> execute();
         
